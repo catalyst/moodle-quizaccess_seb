a22c4319c0ec0a2008d0707210818bf13e9ebbc0
Andrew Madden <andrew.madden@live.com.au>
2019-12-16 15:27:53 +1100

    Issue #14 Add extra tests and use real data extracted from Safe Exam Browser to test hash creation.

--- tests/phpunit/config_key_test.php
+++ tests/phpunit/config_key_test.php
@@ -31,10 +31,35 @@ class quizaccess_seb_config_key_testcase extends advanced_testcase {
 
     /**
      * Test that the Config Key hash is generated.
+     *
+     * The file was generated using SEB 2.1.4 (Mac OS), and the expected Config Key extracted from the config tool.
      */
     public function test_config_key_hash_generated() {
-        $xml = file_get_contents(__DIR__ . '/sample_data/unencrypted.seb');
+        $xml = file_get_contents(__DIR__ . '/sample_data/unencrypted_mac_001.seb');
         $hash = config_key::generate($xml)->get_hash();
-        $this->assertEquals('9683df435f3a226d9d8b206846910ff1045c18b13e518474541f40461a61afee', $hash);
+        $this->assertEquals('4fa9af8ec8759eb7c680752ef4ee5eaf1a860628608fccae2715d519849f9292', $hash);
+    }
+
+    /**
+     * Test that trying to generate the hash key with bad xml will result in an error.
+     */
+    public function test_config_key_not_generated_with_bad_xml() {
+        set_error_handler(function($errno, $errstr, $errfile, $errline ){
+            throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
+        });
+        $this->expectException(ErrorException::class);
+        $this->expectExceptionMessageRegExp("/.*DOMDocument::loadXML().*/");
+        config_key::generate("<?xml This is some bad xml for sure.");
+    }
+
+    /**
+     * Check that the Config Key hash is not altered if the originatorVersion is present in the XML or not.
+     */
+    public function test_presence_of_originator_version_does_not_effect_hash() {
+        $xmlwithoriginatorversion = file_get_contents(__DIR__ . '/sample_data/simpleunencrypted.seb');
+        $xmlwithoutoriginatorversion = file_get_contents(__DIR__ . '/sample_data/simpleunencryptedwithoutoriginator.seb');
+        $hashwithorigver = config_key::generate($xmlwithoriginatorversion)->get_hash();
+        $hashwithoutorigver = config_key::generate($xmlwithoutoriginatorversion)->get_hash();
+        $this->assertEquals($hashwithorigver, $hashwithoutorigver);
     }
 }
