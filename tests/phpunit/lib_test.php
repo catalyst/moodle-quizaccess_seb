bc64756fe4158e517800ac7de9a8fb4e846d27a9
Andrew Madden <andrew.madden@live.com.au>
2019-12-19 12:49:17 +1100

    Issue #17 Added extra tests including success case

--- tests/phpunit/lib_test.php
+++ tests/phpunit/lib_test.php
@@ -48,7 +48,17 @@ class quizaccess_seb_lib_testcase extends advanced_testcase {
         $this->setUser($user); // Log user in.
 
         $this->assertFalse(serve_quiz_config_xml('999'));
-        $this->assertDebuggingCalled();
+        $this->assertDebuggingCalled("Can't find data record in database. (SELECT cm.*, m.name, md.name AS modname \n"
+                . "              FROM {course_modules} cm\n"
+                . "                   JOIN {modules} md ON md.id = cm.module\n"
+                . "                   JOIN {quiz} m ON m.id = cm.instance\n"
+                . "                   \n"
+                . "             WHERE cm.id = :cmid AND md.name = :modulename\n"
+                . "                   \n"
+                . "[array (\n"
+                . "  'cmid' => '999',\n"
+                . "  'modulename' => 'quiz',\n"
+                .')])', DEBUG_DEVELOPER);
     }
 
     /**
@@ -61,6 +71,70 @@ class quizaccess_seb_lib_testcase extends advanced_testcase {
         $this->setUser($user); // Log user in.
 
         $this->assertFalse(serve_quiz_config_xml($quiz->cmid));
-        $this->assertDebuggingCalled();
+        $this->assertDebuggingCalled('Unsupported redirect detected, script execution terminated', DEBUG_DEVELOPER);
+    }
+
+    /**
+     * Test that if SEB quiz settings can't be found, a file will not be served.
+     */
+    public function test_file_not_served_if_config_not_found_for_cmid() {
+        global $DB;
+        $user = $this->getDataGenerator()->create_user();
+        $course = $this->getDataGenerator()->create_course();
+        $quiz = $this->getDataGenerator()->create_module('quiz', ['course' => $course->id]);
+        $this->getDataGenerator()->enrol_user($user->id, $course->id);
+        $this->setUser($user); // Log user in.
+
+        $this->assertTrue($DB->delete_records('quizaccess_seb_quizsettings', ['quizid' => $quiz->id]));
+
+        $this->assertFalse(serve_quiz_config_xml($quiz->cmid));
+        $this->assertDebuggingCalled('quizaccess_seb - Could not find SEB config for quiz with cmid: ' . $quiz->cmid,
+            DEBUG_DEVELOPER);
+    }
+
+    /**
+     * That that if config is empty for a quiz, a file will not be served.
+     */
+    public function test_file_not_served_if_config_empty() {
+        global $DB;
+        $user = $this->getDataGenerator()->create_user();
+        $course = $this->getDataGenerator()->create_course();
+        $quiz = $this->getDataGenerator()->create_module('quiz', ['course' => $course->id]);
+        $this->getDataGenerator()->enrol_user($user->id, $course->id);
+        $this->setUser($user); // Log user in.
+
+        // Update database with empty config.
+        $settingsrecord = $DB->get_record('quizaccess_seb_quizsettings', ['quizid' => $quiz->id], '*');
+        $settingsrecord->config = '';
+        $this->assertTrue($DB->update_record('quizaccess_seb_quizsettings', $settingsrecord));
+
+        $this->assertFalse(serve_quiz_config_xml($quiz->cmid));
+        $this->assertDebuggingCalled('quizaccess_seb - Could not find SEB config for quiz with cmid: ' . $quiz->cmid,
+            DEBUG_DEVELOPER);
+    }
+
+    /**
+     * Test file is served successfully.
+     *
+     * To prevent headers throwing error, run this test in a separate process.
+     *
+     * @runInSeparateProcess
+     */
+    public function test_file_served() {
+        $user = $this->getDataGenerator()->create_user();
+        $course = $this->getDataGenerator()->create_course();
+        $quiz = $this->getDataGenerator()->create_module('quiz', ['course' => $course->id]);
+        $this->getDataGenerator()->enrol_user($user->id, $course->id);
+        $this->setUser($user); // Log user in.
+
+        $this->expectOutputString("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                . "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n"
+                . "<plist version=\"1.0\"><dict><key>showTaskBar</key><true/><key>allowWlan</key><false/>"
+                . "<key>showReloadButton</key><true/><key>showTime</key><true/><key>showInputLanguage</key><true/>"
+                . "<key>allowQuit</key><true/><key>quitURLConfirm</key><true/><key>audioControlEnabled</key><false/>"
+                . "<key>audioMute</key><false/><key>allowSpellCheck</key><false/><key>browserWindowAllowReload</key><true/>"
+                . "<key>URLFilterEnable</key><false/><key>URLFilterEnableContentFilter</key><false/>"
+                . "<key>URLFilterRules</key><array/></dict></plist>\n");
+        $this->assertTrue(serve_quiz_config_xml($quiz->cmid));
     }
 }
