9c4e6f17a0acbe491a260d31279f6b35a9f269b0
Nicholas Hoobin <nicholashoobin@catalyst-au.net>
2020-02-12 14:55:35 +1100

    Issue #140 Bugfix for creating a new template, it is now no longer locked

--- classes/template.php
+++ classes/template.php
@@ -131,7 +131,7 @@ class template extends \core\persistent {
      * @return bool
      */
     public function can_delete() : bool {
-        $result = false;
+        $result = true;
 
         if ($this->get('id')) {
             $settings = quiz_settings::get_records(['templateid' => $this->get('id')]);
