a9a030c9df6318c367f31ba2601d36db4109e7a8
Nicholas Hoobin <nicholashoobin@catalyst-au.net>
2020-02-20 15:19:27 +1100

    Issue #165 Render Quit SEB element as div/href instead of form/button.
    This now acts as a SEB Quit link, instead of posting to the page.

--- rule.php
+++ rule.php
@@ -361,8 +361,8 @@ class quizaccess_seb extends quiz_access_rule_base {
 
         // Only display if the link has been configured and attempts are greater than 0.
         if ($quizsettings->get('linkquitseb')) {
-            $url = $quizsettings->get('linkquitseb');
-            $quitbutton = $OUTPUT->single_button($url, get_string('exitsebbutton', 'quizaccess_seb'), 'get');
+            $content = html_writer::link($quizsettings->get('linkquitseb'), get_string('exitsebbutton', 'quizaccess_seb'));
+            $quitbutton = html_writer::div($content, 'btn btn-secondary');
         }
 
         return $quitbutton;
