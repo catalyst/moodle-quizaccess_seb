d16f547d03b88fcefda9a1f6aeb124c004b72015
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2020-03-27 18:58:17 +1100

    Version bump

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020032600;
-$plugin->release = 2020032600; // Match to version.
+$plugin->version = 2020032700;
+$plugin->release = 2020032700; // Match to version.
 $plugin->requires = 2019052000; // Moodle 3.7.
 $plugin->component = 'quizaccess_seb';
 $plugin->maturity = MATURITY_ALPHA;
